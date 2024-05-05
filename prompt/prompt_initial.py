200a60fc02b9fff1e31dab1542fc98ac74ea038d
yeren66 <794493836@qq.com>
2024-05-06 01:57:18 +0800

    add prompt part

---
+++ prompt/prompt_initial.py
@@ -0,0 +1,160 @@
+def prompt_role():
+    prompt = "You are an excellent programmer of drone controller for flying tasks. "
+    prompt += "Given an instruction and information about the working environment, "
+    prompt += "you break it down into a sequence of flying control codes. "
+    prompt += "Please do not begin working until I say \"Start working.\" "
+    prompt += "Instead, simply output the message \"Waiting for next input.\" Understood?"
+
+    return prompt
+
+def prompt_env():
+    prompt = "Information about environments and objects are given as python dictionary. Example:\n"
+    prompt += """\"\"\"
+{"environment size": "length * width * height", 
+"block1": (x1:x2, y1:y2, z1:z2),
+"block2": (x1:x2, y1:y2, z1:z2),
+"start point": (x, y, z),
+"destination": (x, y, z)}
+\"\"\"
+-------------------------------------------------------
+The texts above are part of the overall instruction. Do not start working yet:
+"""
+
+    return prompt
+
+def prompt_function():
+    prompt = "Necessary and sufficient flying control codes are defined as follows:\n"
+    prompt += """\"\"\"
+"FLY CONTROL CODES LIST"
+takeOff(drone_number, height): Make the No.drone_number(default 0) drone take off to height(z-axis).
+moveCtrl(drone_number, direction, distance): Make the No.drone_number(default 0) drone move in direction for distance. direction is 0 for x, 1 for y, 2 for z, 3 for -x, 4 for -y, 5 for -z.
+flyCtrl(drone_number, height): Make the No.drone_number(default 0) drone landing to height(default 0).
+\"\"\"
+-------------------------------------------------------
+The texts above are part of the overall instruction. Do not start working yet:
+"""
+    return prompt
+
+def prompt_output_format():
+    prompt = "You divide the task into a sequence of flying control codes and put them together as a python dictionary.\n"
+    prompt += """The dictionary has five keys.
+\"\"\"
+- dictionary["task_cohesion"]: A dictionary containing information about the flying control codes that have been split up.
+- dictionary["drone_before"]: The position of the drone before the manipulation.
+- dictionary["drone_after"]: The position of the drone after the manipulation.
+- dictionary["instruction_summary"]: contains a brief summary of the given sentence.
+- dictionary["question"]: If you cannot understand the given sentence, you can ask the user to rephrase the sentence. Leave this key empty if you can understand the given sentence.
+\"\"\"
+Three keys exist in dictionary["task_cohesion"].
+\"\"\"
+- dictionary["task_cohesion"]["task_sequence"]: A list of flying control codes. Only the behaviors defined in the "FLY CONTROL CODES LIST" will be used.
+- dictionary["task_cohesion"]["step_instructions"]: contains a list of instructions for the drone corresponding to the list of flying control codes.
+- dictionary["task_cohesion"]["drone_position"]: The position of the drone after performing per flying control code. 
+\"\"\"
+-------------------------------------------------------
+The texts above are part of the overall instruction. Do not start working yet:
+"""
+
+    return prompt
+
+def prompt_example():
+    prompt = "I will give you some examples of the input and the output you will generate. \n"
+    prompt += """Example1:
+\"\"\"
+- Input:
+{"environment size": "100 * 100 * 100",
+"block1": (40:60, 40:60, 0:100),
+"start point": (0, 0, 0),
+"destination": (100, 100, 0)}
+Instructions: "Fly to the destination avoiding the block."
+- Output:
+{"task_cohesion": 
+    {"task_sequence": [
+        "takeOff(0, 50)", 
+        "moveCtrl(0, 0, 100)", 
+        "moveCtrl(0, 1, 100)", 
+        "flyCtrl(0, 0)"
+        ],
+    "step_instructions": [
+        "Take off to 50cm height.",
+        "Move 100cm to the x direction.",
+        "Move 100cm to the y direction.",
+        "Land."
+        ],
+    "drone_position": [
+        (0, 0, 50),
+        (100, 0, 50),
+        (100, 100, 50),
+        (100, 100, 0)
+    ]
+    },
+    "drone_before": (0, 0, 0),
+    "drone_after": (100, 100, 0),
+    "instruction_summary": "Fly to the destination avoiding the block.",
+    "question": ""
+    }
+\"\"\"
+""" 
+    prompt += """Example2:
+\"\"\"
+- Input:
+{
+    "environment size": "200 * 200 * 200",
+    "block1": "(0:150, 50:150, 0:150)",
+    "block2": "(160:180, 0:100, 0:200)",
+    "start point": "(0, 0, 0)",
+    "destination": "(200, 0, 0)"
+}
+Instructions: "Fly to the destination avoiding the block."
+
+- Output:
+{
+    "task_cohesion": 
+    {
+        "task_sequence": [
+            "takeOff(0, 60)", 
+            "moveCtrl(0, 0, 150)", 
+            "moveCtrl(0, 1, 120)", 
+            "moveCtrl(0, 0, 50)",
+            "moveCtrl(0, 4, 120)",
+            "flyCtrl(0, 0)"
+        ],
+        "step_instructions": [
+            "Take off to 60cm height.",
+            "Move 150cm in the x direction.",
+            "Move 120cm in the y direction.",
+            "Move 50cm in the x direction.",
+            "Move 120cm in the -y direction.",
+            "Land."
+        ],
+        "drone_position": [
+            "(0, 0, 60)",
+            "(150, 0, 60)",
+            "(150, 120, 60)",
+            "(200, 120, 60)",
+            "(200, 0, 60)",
+            "(200, 0, 0)"
+        ]
+    },
+    "drone_before": "(0, 0, 0)",
+    "drone_after": "(200, 0, 0)",
+    "instruction_summary": "Fly to the destination avoiding the blocks.",
+    "question": ""
+}
+"""
+
+    prompt += """-------------------------------------------------------
+The texts above are part of the overall instruction. Do not start working yet:
+"""
+    return prompt
+
+def common_gpt_reply():
+    return "Understood. I will wait for further instructions before starting to work."
+
+if __name__ == "__main__":
+    # print(prompt_role())
+    # print(prompt_env())
+    # print(prompt_function())
+    # print(prompt_output_format())
+    print(prompt_example())
+    # print(common_gpt_reply())
---
+++ prompt/prompt_query.py
@@ -0,0 +1,60 @@
+
+
+
+# Input Framework is like this:
+# - Input:
+# {"environment size": "100 * 100 * 100",
+# "block1": (40:60, 40:60, 0:100),
+# "start point": (0, 0, 0),
+# "destination": (100, 100, 0)}
+# Instructions: "Fly to the destination avoiding the block."
+# - Output:
+# {"task_cohesion": 
+#     {"task_sequence": [
+#         "takeOff(0, 50)", 
+#         "moveCtrl(0, 0, 100)", 
+#         "moveCtrl(0, 1, 100)", 
+#         "flyCtrl(0, 0)"
+#         ],
+#     "step_instructions": [
+#         "Take off to 50cm height.",
+#         "Move 100cm to the x direction.",
+#         "Move 100cm to the y direction.",
+#         "Land."
+#         ]
+#     },
+#     "drone_before": (0, 0, 0),
+#     "drone_after": (100, 100, 0),
+#     "instruction_summary": "Fly to the destination avoiding the block.",
+#     "question": ""
+#     }
+
+
+def task1():
+    prompt = """Start working. Resume from the environment below.
+\"\"\"
+{"environment size": "100 * 100 * 100",
+"block1": (0:60, 20:40, 0:100),
+"block2": (40:100, 60:80, 0:100),
+"start point": (0, 0, 0),
+"destination": (100, 100, 0)}
+\"\"\"
+The instruction is as follows:
+\"\"\"
+Instructions: "Fly to the destination avoiding the block."
+\"\"\"
+The dictionary that you return should be formatted as python dictionary. Follow these rules:
+1. The first element should be takeOff() to take off.
+2. The last element should be flyCtrl() to land.
+3. Make sure that each element of the ["step_instructions"] explains corresponding element of the ["task_sequence"]. Refer to "FLY CONTROL CODES LIST" to understand the elements of ["task_sequence"]
+4. The length of the ["step_instructions"] list must be the same as the length of the ["task_sequence"] list.
+5. Never left ',' at the end of the list.
+6. All keys of the dictionary should be double-quoted.
+7. Insert ``` at the beginning and the end of the dictionary to separate it from the rest of your response.
+Adhere to the output format I defined above. Think step by step. Good luck!
+"""
+
+    return prompt
+
+if __name__ == "__main__":
+    print(task1())
